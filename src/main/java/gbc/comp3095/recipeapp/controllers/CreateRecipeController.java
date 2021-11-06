b7439145849f673fb4503f000a3621c67e073539
sparksflyupwards <saad.jamil.khan@gmail.com>
2021-11-06 15:46:59 -0400

    controller now adds recipes to repository and redirects user to recipes list

--- src/main/java/gbc/comp3095/recipeapp/controllers/CreateRecipeController.java
+++ src/main/java/gbc/comp3095/recipeapp/controllers/CreateRecipeController.java
@@ -1,9 +1,13 @@
 package gbc.comp3095.recipeapp.controllers;
 
+import gbc.comp3095.recipeapp.models.Recipe;
+import gbc.comp3095.recipeapp.models.User;
 import gbc.comp3095.recipeapp.services.Implementations.recipe.RecipeServiceImpl;
+import gbc.comp3095.recipeapp.services.Implementations.user.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,15 +15,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CreateRecipeController {
 
     private final RecipeServiceImpl recipeService;
+    private final UserServiceImpl userService;
     @Autowired
-    public CreateRecipeController(RecipeServiceImpl recipeService) {
+    public CreateRecipeController(RecipeServiceImpl recipeService, UserServiceImpl userService) {
         this.recipeService = recipeService;
+        this.userService = userService;
     }
 
     @RequestMapping("/addrecipe")
     public String getRecipes(Model model){
-        model.addAttribute("recipes", recipeService.findAll());
+        model.addAttribute("recipe", new Recipe());
         return "recipes/createRecipe";
+    }
+
+    @PostMapping("/addrecipe")
+    public String addRecipe(@ModelAttribute Recipe recipe, Model model) {
+        model.addAttribute("recipe", recipe);
+        User user = userService.findById(1L).get();
+        userService.createRecipe(user, recipe);
+
+        return "redirect:/recipes";
     }
 
 
