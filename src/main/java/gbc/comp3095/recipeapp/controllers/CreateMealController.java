af73f23c578eab8a2b16cdc91caa1d702022c3ab
cyberant28 <73520326+cyberant28@users.noreply.github.com>
2021-12-05 17:27:16 -0500

    split the modal functionality into pages - read description
    - split the recipe controller into view, edit recipe
    - split the meal controller into view, edit, create meals
    - replaced edit/view/create modals into their own pages

--- src/main/java/gbc/comp3095/recipeapp/controllers/CreateMealController.java
+++ src/main/java/gbc/comp3095/recipeapp/controllers/CreateMealController.java
@@ -1,18 +1,9 @@
 /*********************************************************************************
- * Project: < COMP3095_Assignment1 >
- * Assignment: < assignment 1 >
- * Author(s): < Fred Pedersen >
- * Student Number: < 101378456 >
- * Date: 07-11-2021
- * Description: The PlannedMealController make sure that the right data from the model layer is being parsed to the right html page
- *********************************************************************************/
-/*********************************************************************************
  * Project: < COMP3095_Assignment2 >
  * Assignment: < assignment 2 >
  * Author(s): < Gustavo Beltran >
  * Student Number: < 101225087 >
  * Date: 12-05-2021
- * Description: Split the controller into Create Meal and Edit Meal Controllers
  *********************************************************************************/
 package gbc.comp3095.recipeapp.controllers;
 
@@ -20,34 +11,55 @@ import gbc.comp3095.recipeapp.models.Meal;
 import gbc.comp3095.recipeapp.models.Recipe;
 import gbc.comp3095.recipeapp.models.User;
 import gbc.comp3095.recipeapp.services.Implementations.meal.MealServiceImpl;
+import gbc.comp3095.recipeapp.services.Implementations.recipe.RecipeServiceImpl;
 import gbc.comp3095.recipeapp.services.Implementations.user.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-public class MealController {
+public class CreateMealController {
 
     private final MealServiceImpl mealService;
     private final UserServiceImpl userService;
 
 
-    public MealController(MealServiceImpl mealService, UserServiceImpl userService) {
+    public CreateMealController(MealServiceImpl mealService, UserServiceImpl userService) {
         this.mealService = mealService;
         this.userService = userService;
     }
 
-    @RequestMapping("/meals")
+    @RequestMapping("/addmeal")
     public String getMeals(Model model){
         model.addAttribute("meals", mealService.findAll());
         model.addAttribute("meal", new Meal());
         User user = userService.findById(1L).get();
         model.addAttribute("recipes", user.getRecipes());
-        return "meals/list";
-    }
+        return "meals/createMeal";    }
 
+
+
+//    @PostMapping("/editmeal/{id}")
+//    public String editMeal(@ModelAttribute Meal meal, @PathVariable("id") String pathId, Model model) {
+//        model.addAttribute("meal", meal);
+//        User user = userService.findById(1L).get();
+//        model.addAttribute("recipes", user.getRecipes());
+//        try{
+//            Long mealId = Long.valueOf(pathId);
+//            mealService.updateMealTitle(mealId, meal.getTitle());
+//        }
+//        catch (Exception exception){
+//            throw new RuntimeException("Invalid id in path");
+//        }
+//        return "redirect:/meals";
+//    }
+
+    @PostMapping(value = "/addmeal", params = {"title", ""})
+    public String addMeal(@ModelAttribute Meal meal, Model model) {
+        model.addAttribute("meal", meal);
+        User user = userService.findById(1L).get();
+        userService.createMeal(user, meal);
+        return "redirect:/meals";
+    }
 }
--- src/main/java/gbc/comp3095/recipeapp/controllers/EditMealController.java
+++ src/main/java/gbc/comp3095/recipeapp/controllers/EditMealController.java
@@ -1,53 +1,66 @@
 /*********************************************************************************
- * Project: < COMP3095_Assignment1 >
- * Assignment: < assignment 1 >
- * Author(s): < Fred Pedersen >
- * Student Number: < 101378456 >
- * Date: 07-11-2021
- * Description: The PlannedMealController make sure that the right data from the model layer is being parsed to the right html page
- *********************************************************************************/
-/*********************************************************************************
  * Project: < COMP3095_Assignment2 >
  * Assignment: < assignment 2 >
  * Author(s): < Gustavo Beltran >
  * Student Number: < 101225087 >
  * Date: 12-05-2021
- * Description: Split the controller into Create Meal and Edit Meal Controllers
  *********************************************************************************/
+
 package gbc.comp3095.recipeapp.controllers;
 
 import gbc.comp3095.recipeapp.models.Meal;
 import gbc.comp3095.recipeapp.models.Recipe;
 import gbc.comp3095.recipeapp.models.User;
 import gbc.comp3095.recipeapp.services.Implementations.meal.MealServiceImpl;
+import gbc.comp3095.recipeapp.services.Implementations.recipe.RecipeServiceImpl;
 import gbc.comp3095.recipeapp.services.Implementations.user.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-public class MealController {
+public class EditMealController {
 
     private final MealServiceImpl mealService;
     private final UserServiceImpl userService;
 
 
-    public MealController(MealServiceImpl mealService, UserServiceImpl userService) {
+    public EditMealController(MealServiceImpl mealService, UserServiceImpl userService) {
         this.mealService = mealService;
         this.userService = userService;
     }
 
-    @RequestMapping("/meals")
+    @RequestMapping("/editmeal")
     public String getMeals(Model model){
         model.addAttribute("meals", mealService.findAll());
         model.addAttribute("meal", new Meal());
         User user = userService.findById(1L).get();
         model.addAttribute("recipes", user.getRecipes());
-        return "meals/list";
+        return "meals/editmeal";    }
+
+
+
+    @PostMapping("/editmeal/{id}")
+    public String editMeal(@ModelAttribute Meal meal, @PathVariable("id") String pathId, Model model) {
+        model.addAttribute("meal", meal);
+        User user = userService.findById(1L).get();
+        model.addAttribute("recipes", user.getRecipes());
+        try{
+            Long mealId = Long.valueOf(pathId);
+            mealService.updateMealTitle(mealId, meal.getTitle());
+        }
+        catch (Exception exception){
+            throw new RuntimeException("Invalid id in path");
+        }
+        return "redirect:/meals";
     }
 
+//    @PostMapping(value = "/addmeal", params = {"title", ""})
+//    public String addMeal(@ModelAttribute Meal meal, Model model) {
+//        model.addAttribute("meal", meal);
+//        User user = userService.findById(1L).get();
+//        userService.createMeal(user, meal);
+//        return "redirect:/meals";
+//    }
 }
